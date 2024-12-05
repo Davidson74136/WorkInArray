ad39eeb9a332a614283bf6261a8d0a002aed16a6
David <davidgabaraev32@gmail.com>
2024-12-05 21:25:50 +0300

    Init

--- WorkInArray.cpp
+++ WorkInArray.cpp
@@ -1,15 +1,31 @@
 ﻿#include <iostream>
 using std::cout;
-int main()
+
+void show_array(int* m,int s)
 {
-	const int SIZE = 8;
-	int mass[SIZE];
-	for (int i = 0 ; i < SIZE; i++) {
-		mass[i] = SIZE - i;
-	}
-	for (int i = 0; i < SIZE; i++) {
-		cout << mass[i] << ", ";
+	
+	for (int i = 0; i < s; i++) {
+		cout << m[i] << ", ";
 	}
 	cout << (char)8 << (char)8;
+}
+
+//fill array function
+void fill_array(int* m, int sz ) 
+{
+	for (int i = 0; i < sz; i++) {
+		if (i / 2 == (float)i / 2.0)
+		{
+			m[i] = rand() % 1000;
+		}
+	}
+}
+int main()     
+{
+	const int SIZE = 8;
+	int mas[SIZE];
+
+	show_array(mas, SIZE);
+	fill_array(mas, SIZE);
 }
 
